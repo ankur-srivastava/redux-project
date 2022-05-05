040f8165e9c733fef032d8d5931815998d67b851
ankur-srivastava <ankurlkw@gmail.com>
2022-05-05 06:44:47 +0530

    added a new state

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,22 @@
 const redux = require('redux')
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true }
+
+const counterReducer = (state = initialState, action) => {
     if(action.type === 'increment') {
         return {
-            counter: state.counter+action.amount
+            counter: state.counter+action.amount,
+            showCounter: state.showCounter
         }
     } else if(action.type === 'decrement') {
         return {
-            counter: state.counter-action.amount
+            counter: state.counter-action.amount,
+            showCounter: state.showCounter
+        }
+    } else if(action.type === 'toggle') {
+        return {
+            counter: state.counter,
+            showCounter: !state.showCounter
         }
     }
     return state
